e54608361ab63dd01165e84d1ca06e668ddbf88f
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-10 13:44:46 +0100

    created a very basic foundation for a new fragment

--- app/src/main/java/com/example/androidprojectma23/ProfileCreationStep2Fragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ProfileCreationStep2Fragment.kt
@@ -1,4 +1,23 @@
 package com.example.androidprojectma23
 
-class ProfileCreationStep2Fragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ProfileCreationStep2Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        
+        return inflater.inflate(R.layout.fragment_profile_creation_step2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
