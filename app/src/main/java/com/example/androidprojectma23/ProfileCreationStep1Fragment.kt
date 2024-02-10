492b667e9fb2cee4468b80a8b1dd512a5c57eac9
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-10 13:22:46 +0100

    created initUI function thats being called in onViewCreated to keep it clean

--- app/src/main/java/com/example/androidprojectma23/ProfileCreationStep1Fragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ProfileCreationStep1Fragment.kt
@@ -6,30 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 
 class ProfileCreationStep1Fragment : Fragment() {
 
+    // private val database = FirebaseFirestore.getInstance()
+    private lateinit var displayNameEditText: EditText
+    private lateinit var nextStepButton: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_profile_creation_step1, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initUI(view)
+    }
 
-        view.findViewById<Button>(R.id.button_profile_creation_nextStep).setOnClickListener {
-            // TODO: Fetch user ID from firebase authentication
-            // val userId = Firebase.auth.currentUser?.uid
+    private fun initUI(view: View) {
+        displayNameEditText = view.findViewById(R.id.displayName_textInputEditText)
+        nextStepButton = view.findViewById(R.id.button_profile_creation_nextStep)
 
-            // TODO: Save profile image and display name in firestore
-            // saveProfileImage(userId, imageUri)
-            // saveProfileDisplayName(userId, displayName)
-
+        nextStepButton.setOnClickListener {
+            val displayName = displayNameEditText.text.toString().trim()
+            if (displayName.isNotEmpty()) {
+               // saveDisplayNameToFirestore(displayName)
+            } else {
+                // Visa felmeddelande om displaynamnet är tomt
+            }
         }
     }
 
