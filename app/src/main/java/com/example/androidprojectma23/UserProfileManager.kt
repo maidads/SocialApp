4c496d215e0976832e4e2e9f0b33fa0ceeb140ac
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-11 09:08:29 +0100

    created a class to handle all firebase operations, starting with 2 functions for user image and display name

--- app/src/main/java/com/example/androidprojectma23/UserProfileManager.kt
+++ app/src/main/java/com/example/androidprojectma23/UserProfileManager.kt
@@ -1,4 +1,26 @@
 package com.example.androidprojectma23
 
-class UserProfileManager {
+import android.net.Uri
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.StorageReference
+
+class UserProfileManager(private val storageRef: StorageReference, private val firestore: FirebaseFirestore) {
+
+    fun uploadProfileImage(userId: String, imageUri: Uri, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        val ref = storageRef.child("userImages/$userId/profileImage.jpg")
+        ref.putFile(imageUri).addOnSuccessListener {
+            onSuccess()
+        }.addOnFailureListener {
+            onFailure(it)
+        }
+    }
+
+    fun updateDisplayName(userId: String, displayName: String, onSuccess: () -> Unit, onFailure: (Exception) -> Unit) {
+        val userDocumentRef = firestore.collection("users").document(userId)
+        userDocumentRef.update("displayName", displayName).addOnSuccessListener {
+            onSuccess()
+        }.addOnFailureListener {
+            onFailure(it)
+        }
+    }
 }
