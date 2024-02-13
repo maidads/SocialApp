c741af6fb96f9f181e34fb64b26fa5118c7916b3
Ina Burström <inaburstrom@hotmail.com>
2024-02-13 13:14:24 +0100

    Adding variables in User class.

--- app/src/main/java/com/example/androidprojectma23/User.kt
+++ app/src/main/java/com/example/androidprojectma23/User.kt
@@ -1,6 +1,9 @@
 package com.example.androidprojectma23
 
 data class User(
-    val displayName: String
+    val displayName: String,
+    val profileImage: String,  // URL for profile pic
+    val interests: String,  // Interests, showing as icons
+    val age: String
 )
 
