240ab2270bdd86a8dc9b7c915b3420e6ca51058f
Ina Burström <inaburstrom@hotmail.com>
2024-02-16 08:09:31 +0100

    Replaced map in ProfileCreationStep2Fragment.kt with new  iconIdToDocIdMap.

--- app/src/main/java/com/example/androidprojectma23/ProfileCreationStep2Fragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ProfileCreationStep2Fragment.kt
@@ -22,29 +22,6 @@ class ProfileCreationStep2Fragment : Fragment() {
     private lateinit var userId: String
 
     private var selectedInterest = mutableListOf<Int>()
-    private val imageViewIdToFirestoreDocumentIdMap = mapOf(
-
-        R.id.icon_music to "qWAkLQAUlXIuJCn45ChZ",
-        R.id.icon_sports to "9NPO76LYaq9hl5KOCtC4",
-        R.id.icon_movies to "AYpDbrWtQOUOt7rBDXDH",
-        R.id.icon_art to "iglkcuMPG8egGg4scETR",
-        R.id.icon_books to "YmMikFDeggctuiqSYrmw",
-        R.id.icon_wine to "YvVGXixVaQSsMhAZaCy2",
-        R.id.icon_cooking to "HPQHhJeFC7wQaHyAFSnU",
-        R.id.icon_travel to "M9RqxG3Caa0JNT9h6ZTX",
-        R.id.icon_festival to "EymGn10U227Gf5xmducS",
-        R.id.icon_fashion to "vs5sifFqzkrCyVILxya6",
-        R.id.icon_dance to "zcz594bv81UYIWjhWgTy",
-        R.id.icon_games to "SM8Oh6Hnba6Gzjpn77RJ",
-        R.id.icon_yoga to "6O7GXIC0DWKz6T8wXCJa",
-        R.id.icon_camping to "HvJnJ1QKuS2l9IAzYkGa",
-        R.id.icon_fika to "0YB3cpO2ducwVQeuCmHC",
-        R.id.icon_training to "xZ4sv4Th1Rx3xmUWPr7C",
-        R.id.icon_animals to "YeByZ6w5see5N6GfBPYI",
-        R.id.icon_garden to "zBgJksLJY1Fa0s3oUSvg",
-        R.id.icon_photography to "ftWLcl8ag7pabyuSSJih",
-        R.id.icon_technology to "GTPROJYniNOrFBivL7wE"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -85,7 +62,7 @@ class ProfileCreationStep2Fragment : Fragment() {
             val isSelected = selectedInterest.contains(imageView.id)
 
             // Get document-ID based on ImageView ID
-            val documentId = imageViewIdToFirestoreDocumentIdMap[imageView.id]
+            val documentId = iconIdToDocIdMap[imageView.id]
 
             if (isSelected) {
                 imageView.alpha = 1f
