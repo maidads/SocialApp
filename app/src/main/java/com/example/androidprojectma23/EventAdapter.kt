7393ef99aaf2f1b227ac3b698a9b4edda8c028ab
Maida <98819095+maidads@users.noreply.github.com>
2024-02-16 21:14:42 +0100

    Implemented members for EventAdapter and added a new class EventViewHolder

--- app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
+++ app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.androidprojectma23
 
-class EventAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class EventAdapter(private var events: List<Event>) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+    class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
