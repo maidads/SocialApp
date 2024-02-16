4bb59428f84e627a9c22912cec7f1d78a2ea3937
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-16 09:14:45 +0100

    very basic fragment to start off with, creating the view for the chat cards

--- app/src/main/java/com/example/androidprojectma23/ChatFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ChatFragment.kt
@@ -1,4 +1,34 @@
 package com.example.androidprojectma23
 
-class ChatFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var chatAdapter: ChatAdapter
+    private var chatMessages = mutableListOf<ChatMessage>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
+        recyclerView = view.findViewById(R.id.chat_recycler_view)
+
+        // Skapa några exempelmeddelanden
+        chatMessages.add(ChatMessage("Användare 1", "Hej där!", "10:00"))
+        chatMessages.add(ChatMessage("Användare 2", "Hallå!", "10:01"))
+
+        chatAdapter = ChatAdapter(chatMessages)
+        recyclerView.adapter = chatAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
 }
