89e069e19977a109214e59ce097679011586b9b7
Maida <98819095+maidads@users.noreply.github.com>
2024-02-17 16:22:33 +0100

    Declared a variable recyclerView

--- app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 
 class EventPageFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
