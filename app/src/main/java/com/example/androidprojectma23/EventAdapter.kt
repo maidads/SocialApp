5d76b031ee364f0c8d95027910935c6a275f96cc
Maida <98819095+maidads@users.noreply.github.com>
2024-02-17 16:12:46 +0100

    EventViewHolder done to find the items in every event

--- app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
+++ app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
@@ -2,11 +2,15 @@ package com.example.androidprojectma23
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class EventAdapter(private var events: List<Event>) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
     class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val eventNameTextView: TextView = itemView.findViewById(R.id.eventNameTextView)
+
         fun bind(event: Event) {
+            eventNameTextView.text = event.name
         }
     }
 
