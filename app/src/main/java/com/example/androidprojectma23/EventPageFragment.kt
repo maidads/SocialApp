52cbe79ec3dcd989c0898bd12bb8bd5ca5119671
Maida <98819095+maidads@users.noreply.github.com>
2024-02-17 16:28:37 +0100

    Wrote down fun onViewCreated with empty body

--- app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
@@ -14,15 +14,16 @@ class EventPageFragment : Fragment() {
     private lateinit var adapter: EventAdapter
     private lateinit var events: List<Event>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_event_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
