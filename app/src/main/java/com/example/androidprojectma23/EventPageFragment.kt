cfce7e2a4303b6f30647c5f17d7b6d03323e1e81
Maida <98819095+maidads@users.noreply.github.com>
2024-02-17 16:44:41 +0100

    fun fetchEvents done to get events from the database

--- app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/EventPageFragment.kt
@@ -1,6 +1,7 @@
 package com.example.androidprojectma23
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,5 +36,18 @@ class EventPageFragment : Fragment() {
         firestore = FirebaseFirestore.getInstance()
 
     }
+
+    private fun fetchEvents() {
+        firestore.collection("Events Collection")
+            .get()
+            .addOnSuccessListener { snapshot ->
+                val eventsList = snapshot.toObjects(Event::class.java)
+                adapter.updateEvents(eventsList)
+            }
+            .addOnFailureListener { exception ->
+                Log.d("EventPageFragment", "Error getting documents: ", exception)
+            }
+    }
+
 
 }
