406cf842cb276d95110cdab4b2bc5730f4af8d50
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-19 09:24:56 +0100

    hard coded users for the chat fragment for testing purpose

--- app/src/main/java/com/example/androidprojectma23/ChatFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ChatFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class ChatFragment : Fragment() {
+class ChatFragment : Fragment(), ChatAdapter.ChatCardListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var chatAdapter: ChatAdapter
@@ -21,14 +21,34 @@ class ChatFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_chat, container, false)
         recyclerView = view.findViewById(R.id.chat_recycler_view)
 
-        // Skapa några exempelmeddelanden
-        chatMessages.add(ChatMessage("Användare 1", "Hej där!", "10:00"))
-        chatMessages.add(ChatMessage("Användare 2", "Hallå!", "10:01"))
+        // Creating hardcoded examples
+        chatMessages.add(ChatMessage("Användare 1", "Hej där!", "10:00", false))
+        chatMessages.add(ChatMessage("Användare 2", "Hallå!", "10:01", false))
 
-        chatAdapter = ChatAdapter(chatMessages)
+        chatAdapter = ChatAdapter(chatMessages, this)
         recyclerView.adapter = chatAdapter
         recyclerView.layoutManager = LinearLayoutManager(context)
 
         return view
+    }
+
+    override fun onChatCardClicked(position: Int) {
+        val chatMessage = chatMessages[position]
+        openChatConversationFragment(chatMessage)
+    }
+
+    private fun openChatConversationFragment(chatMessage: ChatMessage) {
+        val chatConversationFragment = ChatConversationFragment().apply {
+            arguments = Bundle().apply {
+                // Antag att vi skickar användarnamnet som ett sätt att identifiera konversationen
+                // I en riktig applikation kan detta vara ett unikt ID för konversationen eller användaren
+                putString("conversationUserName", chatMessage.userName)
+            }
+        }
+
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragmentHolder, chatConversationFragment)
+            .addToBackStack(null)  // Lägger till transaktionen till backstack för att möjliggöra navigering tillbaka
+            .commit()
     }
 }
