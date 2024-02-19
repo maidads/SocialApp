b6febcd21a616b5128f2072f80029ccca3296137
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-19 09:26:25 +0100

    basic data structure for the chat message

--- app/src/main/java/com/example/androidprojectma23/ChatMessage.kt
+++ app/src/main/java/com/example/androidprojectma23/ChatMessage.kt
@@ -3,5 +3,6 @@ package com.example.androidprojectma23
 data class ChatMessage(
     val userName: String,
     val messageBody: String,
-    val messageTime: String
+    val messageTime: String,
+    val isUserMessage: Boolean
 )
