69bdc01b4b44fec97f961bc8a5e8a4fe4723a8d4
Ina Burström <inaburstrom@hotmail.com>
2024-02-20 00:17:18 +0100

    Changed type from String to Timestamp in messageTime.

--- app/src/main/java/com/example/androidprojectma23/ChatMessage.kt
+++ app/src/main/java/com/example/androidprojectma23/ChatMessage.kt
@@ -1,8 +1,10 @@
 package com.example.androidprojectma23
 
+import com.google.firebase.Timestamp
+
 data class ChatMessage(
     val userName: String,
     val messageBody: String,
-    val messageTime: String,
+    val messageTime: Timestamp,
     val isUserMessage: Boolean
 )
