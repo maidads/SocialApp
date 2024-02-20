aae98789ebe17d31d7f9e31482194551a1da3e7b
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-20 11:55:45 +0100

    adapter for the recyclelist popup menu

---
+++ app/src/main/java/com/example/androidprojectma23/InterestsAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.androidprojectma23
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class InterestsAdapter(private val interests: List<Int>) : RecyclerView.Adapter<InterestsAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val iconView: ImageView = view.findViewById(R.id.iconView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.interest_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.iconView.context
+        val iconResId = interests[position]
+        holder.iconView.setImageResource(iconResId)
+
+    }
+
+    override fun getItemCount() = interests.size
+}
