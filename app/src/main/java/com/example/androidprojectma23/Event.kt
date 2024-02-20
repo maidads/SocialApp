e189a5cc6f9967f8b8794236dc563de54946f68d
Maida <98819095+maidads@users.noreply.github.com>
2024-02-20 13:19:40 +0100

    more data on my data class Event

--- app/src/main/java/com/example/androidprojectma23/Event.kt
+++ app/src/main/java/com/example/androidprojectma23/Event.kt
@@ -3,5 +3,8 @@ package com.example.androidprojectma23
 data class Event(
     val name: String = "",
     val description: String = "",
-    val date: String = ""
+    val date: String = "",
+    val time: String = "",
+    val location: String = "",
+    val imageUrl: String = ""
 )
