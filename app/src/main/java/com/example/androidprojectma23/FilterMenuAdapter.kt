c760aad1ff9ef4a2135b4c3bfd04c8d27e41fc6c
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-20 15:39:07 +0100

    adapter can keep track on how many interests is selection in the filter menu

--- app/src/main/java/com/example/androidprojectma23/FilterMenuAdapter.kt
+++ app/src/main/java/com/example/androidprojectma23/FilterMenuAdapter.kt
@@ -6,41 +6,52 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class FilterMenuAdapter(private val interests: List<Int>) :
-    RecyclerView.Adapter<FilterMenuAdapter.ViewHolder>() {
+class FilterMenuAdapter(
+    private val interests: List<Int>,
+    private val onSelectionChange: (Int) -> Unit
+) : RecyclerView.Adapter<FilterMenuAdapter.ViewHolder>() {
+
+    private var selectedCount = 1
+
+    init {
+        onSelectionChange(selectedCount)
+    }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val iconView: ImageView = view.findViewById(R.id.iconView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.interest_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.interest_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val context = holder.iconView.context
-        val iconResId = interests[position]
+        val item = interests[position]
+        holder.iconView.setImageResource(item)
 
-        holder.iconView.setImageResource(iconResId)
 
-        // Set alpha value based on index position
         if (position == 0) {
-            holder.iconView.alpha = 1.0f
+            holder.iconView.alpha = 1f
         } else {
             holder.iconView.alpha = 0.5f
         }
 
         holder.iconView.setOnClickListener {
-            // Invert the alpha-value when an icon is pressed
-            val currentAlpha = holder.iconView.alpha
-            val newAlpha = if (currentAlpha == 1.0f) 0.5f else 1.0f
-            holder.iconView.alpha = newAlpha
+
+            if (holder.iconView.alpha < 1f) {
+                holder.iconView.alpha = 1f
+                selectedCount++
+            } else {
+                holder.iconView.alpha = 0.5f
+                selectedCount--
+            }
+
+            onSelectionChange(selectedCount)
         }
-
-
     }
 
-    override fun getItemCount() = interests.size
+    override fun getItemCount(): Int = interests.size
 }
+
+
