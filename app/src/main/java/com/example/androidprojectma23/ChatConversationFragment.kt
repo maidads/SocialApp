461f2e7408df17be13ca0d3d6ec2237bc27fddd2
Ina Burström <inaburstrom@hotmail.com>
2024-02-20 12:11:42 +0100

    First version of getting data from Firestore to show conversation.

--- app/src/main/java/com/example/androidprojectma23/ChatConversationFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/ChatConversationFragment.kt
@@ -1,6 +1,7 @@
 package com.example.androidprojectma23
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChatConversationFragment : Fragment() {
 
@@ -27,23 +29,24 @@ class ChatConversationFragment : Fragment() {
         messageInput = view.findViewById(R.id.chat_message_input)
         sendButton = view.findViewById(R.id.chat_send_button)
 
-        val userName = arguments?.getString("conversationUserName") // Fånga upp användarnamnet som skickats som argument
-
-        // Filtrera eller ladda meddelanden baserat på userName här
-        // För nu simulerar vi att alla meddelanden tillhör denna konversation
-        simulateReceivingMessages(userName)
+        // Fetch argument sent from ChatFragment
+        val conversationId = arguments?.getString("conversationId").toString()
+        val conversationProfileImageUrl = arguments?.getString("conversationProfileImageUrl").toString()
+        val conversationUserName = arguments?.getString("conversationUserName").toString()
 
         // Initial setup for RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
         val adapter = ChatConversationAdapter(chatMessages)
         recyclerView.adapter = adapter
 
+        getConversation(conversationId, conversationProfileImageUrl, conversationUserName)
+
         sendButton.setOnClickListener {
             val messageText = messageInput.text.toString()
             if (messageText.isNotEmpty()) {
                 // Antag att alla meddelanden skickade via denna vy är från användaren
-                val newMessage = ChatMessage(userName ?: "Unknown", messageText, "Now", true)
-                chatMessages.add(newMessage)
+                //val newMessage = ChatMessage(userName ?: "Unknown", messageText, "Now", true)
+                //chatMessages.add(newMessage)
                 adapter.notifyItemInserted(chatMessages.size - 1)
                 messageInput.text.clear()
                 recyclerView.scrollToPosition(chatMessages.size - 1)
@@ -53,10 +56,46 @@ class ChatConversationFragment : Fragment() {
         return view
     }
 
-    private fun simulateReceivingMessages(userName: String?) {
-        // Simulera att lägga till tidigare meddelanden för denna konversation
-        chatMessages.add(ChatMessage(userName ?: "Unknown", "Hello, how are you?", "Yesterday", false))
-        chatMessages.add(ChatMessage("You", "I'm good, thanks for asking!", "Yesterday", true))
+    private fun getConversation(conversationId: String, conversationProfileImageUrl: String, conversationUserName: String){
+        val db = FirebaseFirestore.getInstance()
+        db.collection("conversations").document(conversationId).collection("messages")
+            .get()
+            .addOnSuccessListener {result ->
+                val messages = mutableListOf<ChatMessage>()
+
+                for (document in result) {
+                    val userName = ""//om meddelandet skickats av någon annan än mig, sätt conversationUsername annars mitt displayname
+                    val isUserMessage = "" //om det är någon annan än mig som skickat sätt till false
+                    val messageText = document.getString("messageText")
+                    val messageTime = document.getTimestamp("messageTime")
+                    if (messageText != null && messageTime != null) {
+                        val chatMessage = ChatMessage(
+                            userName,
+                            messageText,
+                            messageTime,
+                            conversationProfileImageUrl,
+                            true
+                        )
+                        messages.add(chatMessage)
+                    }
+                }
+                updateRecyclerView(messages)
+            }.addOnFailureListener { exception ->
+                Log.e("!!!", "Error getting documents: ", exception)
+            }
+
     }
+
+    private fun updateRecyclerView(messages: List<ChatMessage>) {
+        chatMessages.clear()
+        chatMessages.addAll(messages)
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+//    private fun simulateReceivingMessages(userName: String?) {
+//        // Simulera att lägga till tidigare meddelanden för denna konversation
+//        chatMessages.add(ChatMessage(userName ?: "Unknown", "Hello, how are you?", "Yesterday", false))
+//        chatMessages.add(ChatMessage("You", "I'm good, thanks for asking!", "Yesterday", true))
+//    }
 }
 
