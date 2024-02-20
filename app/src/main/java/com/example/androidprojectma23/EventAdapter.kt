9b9033dbb927cb77b5c41995c9d61abb638d267d
Maida <98819095+maidads@users.noreply.github.com>
2024-02-20 13:55:35 +0100

    added variable to find imageView in the layout

--- app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
+++ app/src/main/java/com/example/androidprojectma23/EventAdapter.kt
@@ -3,12 +3,15 @@ package com.example.androidprojectma23
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class EventAdapter(private var events: List<Event>) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
     class EventViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val eventNameTextView: TextView = itemView.findViewById(R.id.eventNameTextView)
+        private val eventImageView: ImageView = itemView.findViewById(R.id.eventImageView)
+
 
         fun bind(event: Event) {
             eventNameTextView.text = event.name
