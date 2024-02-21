843dec9631d16a68c0fd14b8b8a5890dbdb5099f
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-21 21:10:30 +0100

    dependency for geohashing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("com.google.android.gms:play-services-auth:21.0.0")
     implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("com.google.android.material:material:1.11.0")
+    implementation ("ch.hsr:geohash:1.4.0")
 
 
 
