a43490b4996b7ec8cbb44f24a3b064380f4ba0f9
Ina Burström <inaburstrom@hotmail.com>
2024-02-21 14:14:55 +0100

    Added updateTopBar function.

--- app/src/main/java/com/example/androidprojectma23/TopBarManager.kt
+++ app/src/main/java/com/example/androidprojectma23/TopBarManager.kt
@@ -1,2 +1,19 @@
 package com.example.androidprojectma23
 
+class TopBarManager {
+
+    fun updateTopBar (fragmentTag: String){
+        when (fragmentTag) {
+            "Fragment1Tag" -> {
+                // Change icons based on which fragment is currently open
+            }
+            "Fragment2Tag" -> {
+
+            }
+            "Fragment3Tag" -> {
+
+            }
+        }
+    }
+
+}
