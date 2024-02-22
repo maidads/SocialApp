d14e6fa08bc708e3616862dcae6c52862eed1b62
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-22 11:47:36 +0100

    a manager that handles everything with location and geohashing

--- app/src/main/java/com/example/androidprojectma23/GeoLocationManager.kt
+++ app/src/main/java/com/example/androidprojectma23/GeoLocationManager.kt
@@ -10,7 +10,9 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import ch.hsr.geohash.GeoHash
+import com.google.android.gms.location.LocationServices
 
 class GeoLocationManager(private val context: Context, private val activity: Activity) {
 
@@ -21,6 +23,7 @@ class GeoLocationManager(private val context: Context, private val activity: Act
     private var locationManager: LocationManager? = null
 
     init {
+        Log.d("!!!", "LocationManager initialized.")
         locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager?
     }
 
@@ -34,21 +37,19 @@ class GeoLocationManager(private val context: Context, private val activity: Act
 
     fun getCurrentLocationHash(onHashReceived: (String) -> Unit) {
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
+            Log.d("!!!", "Location permissions not granted")
             return
+        } else {
+            Log.d("!!!", "Location permissions granted, requesting updates...")
         }
 
-        locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, object : LocationListener {
-            override fun onLocationChanged(location: Location) {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+            location?.let {
                 val geohash = GeoHash.withCharacterPrecision(location.latitude, location.longitude, 12).toBase32()
+                Log.d("!!!", "Location changed: $location")
                 onHashReceived(geohash)
-
-                locationManager?.removeUpdates(this)
             }
-
-            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
-            override fun onProviderEnabled(provider: String) {}
-            override fun onProviderDisabled(provider: String) {}
-        }, null)
+        }
     }
 }
