33ec72fa5b72fbff330d04bbdb1a7ef69016809c
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-25 22:02:38 +0100

    data field for long and lat

--- app/src/main/java/com/example/androidprojectma23/User.kt
+++ app/src/main/java/com/example/androidprojectma23/User.kt
@@ -1,10 +1,13 @@
 package com.example.androidprojectma23
 
 data class User(
+    val userId: String = "",
     val displayName: String = "",
     val profileImage: String = "",
-    val interests: MutableList<String> = mutableListOf(),
-    val commonInterests: MutableList<String> = mutableListOf(),
-    val geohash: String = ""
+    var interests: MutableList<String> = mutableListOf(),
+    var commonInterests: MutableList<String> = mutableListOf(),
+    val geohash: String = "",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
 )
 
