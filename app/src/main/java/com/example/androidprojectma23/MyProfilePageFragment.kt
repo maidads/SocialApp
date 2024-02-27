a5ef25bcd300e53149fee237a654ae7d4c0a2100
Maida <98819095+maidads@users.noreply.github.com>
2024-02-27 14:57:11 +0100

    Initialized elements and added a click listener on "redigera profil" button

--- app/src/main/java/com/example/androidprojectma23/MyProfilePageFragment.kt
+++ app/src/main/java/com/example/androidprojectma23/MyProfilePageFragment.kt
@@ -1,59 +1,37 @@
 package com.example.androidprojectma23
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MyProfilePageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyProfilePageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var profileImageView: ImageView
+    private lateinit var displayNameTextView: TextView
+    private lateinit var ageTextView: TextView
+    private lateinit var aboutInfoTextView: TextView
+    private lateinit var interestsInfoTextView: TextView
+    private lateinit var editProfileButton: ExtendedFloatingActionButton
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_my_profile_page, container, false)
+
+        profileImageView = view.findViewById(R.id.profileImageViewBack)
+        displayNameTextView = view.findViewById(R.id.displayNameTextViewBack)
+        ageTextView = view.findViewById(R.id.ageTextViewBack)
+        aboutInfoTextView = view.findViewById(R.id.aboutInfoTextView)
+        interestsInfoTextView = view.findViewById(R.id.interestsInfoTextView)
+        editProfileButton = view.findViewById(R.id.editProfileButton)
+
+        editProfileButton.setOnClickListener {
         }
+
+        return view
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile_page, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChangeProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyProfilePageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
