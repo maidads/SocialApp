c5d51d5fa0f493f46e437f97315535012f43d223
Linus Ilbratt <linusilbratt@gmail.com>
2024-02-27 18:31:09 +0100

    clear q

--- app/src/main/java/com/example/androidprojectma23/MyMatchesAdapter.kt
+++ app/src/main/java/com/example/androidprojectma23/MyMatchesAdapter.kt
@@ -3,16 +3,25 @@ package com.example.androidprojectma23
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MyMatchesAdapter(private var users: List<User>) : RecyclerView.Adapter<MyMatchesAdapter.UserViewHolder>() {
 
     class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val displayNameTextView: TextView = view.findViewById(R.id.myMatches_userDisplayName)
+        private val userProfileImage: ImageView = view.findViewById(R.id.myMatches_userProfileImage)
 
         fun bind(user: User) {
             displayNameTextView.text = user.displayName
+
+            Glide.with(itemView.context)
+                .load(user.profileImage)
+                .placeholder(R.drawable.profile_image_placeholder)
+                .error(R.drawable.profile_image_placeholder)
+                .into(userProfileImage)
             // Add more views here
         }
     }
